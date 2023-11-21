b60714d2404291e7a14ad5a5d9592777a86ed02c
Gautham Venkataraman <gautham@dexterenergy.nl>
2023-11-21 11:54:26 +0100

    sync

--- neovim.lua
+++ neovim.lua
@@ -1,7 +1,7 @@
 -- Set the leader key.
 -- Must happen before plugins are loaded
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Options
 local o = vim.o
@@ -38,10 +38,18 @@ require("hlslens").setup()
 
 -- Keymaps
 -- local variable to hold some redundant stuff
-local opts = {noremap = true; silent = true}
+local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- make sure space isn't assigned anything else
 keymap("", "<Space>", "<Nop>", opts)
 -- clear highlights on pressing escape twice
-keymap("n", "<Esc><Esc>", ":noh<CR>", {noremap=true})
+keymap("n", "<Esc><Esc>", ":noh<CR>", { noremap = true })
+-- Easier window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+-- Navigate buffers
+keymap("n", "<C-e>", ":bnext<CR>", opts)
+keymap("n", "<C-q>", ":bprevious<CR>", opts)
