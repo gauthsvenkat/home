6dcda1026199c8750782e178df0cbad3ac6e9392
Gautham Venkataraman <gautham@dexterenergy.nl>
2024-01-22 10:46:20 +0100

    load .env if exists

--- config.nix
+++ config.nix
@@ -35,6 +35,7 @@
     initExtra = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       test -f ~/.p10k.zsh && source ~/.p10k.zsh
+      test -f ~/.env && source ~/.env
       alias v="nvim"
     '';
   };
