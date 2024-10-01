2b9e941c23615b71c38da5693d8aecc78c374792
Gautham <gauthsvenkat@gmail.com>
2024-10-01 18:02:03 +0200

    Remove non mac stuff

--- home.nix
+++ home.nix
@@ -1,12 +1,81 @@
 { config, pkgs, ... }:
 
-let
-  linux_flag = builtins.currentSystem == "x86_64-linux";
-in
 {
-  imports = [ ./config.nix ];
+  home.stateVersion = "23.05"; # Dont' change this
 
-  targets.genericLinux.enable = linux_flag;
-  home.username = if linux_flag then "ando" else "gauthsvenkat";
-  home.homeDirectory = if linux_flag then "/home/ando" else "/Users/gauthsvenkat";
+  targets.genericLinux.enable = true;
+  home.username = "ando";
+  home.homeDirectory = "/home/ando";
+
+  programs.home-manager.enable = true;
+  fonts.fontconfig.enable = true;
+
+  # packages
+  home.packages = with pkgs; [
+    fastfetch
+    pre-commit
+    tree
+    bat
+    meslo-lgs-nf
+    ripgrep
+    cmake
+    nodejs
+    rustup
+    xclip
+    lazygit
+    cue
+  ];
+
+  # autojump
+  programs.autojump = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  # git
+  programs.git = {
+    enable = true;
+    userEmail = "gauthsvenkat@gmail.com";
+    userName = "Gautham";
+  };
+
+  # fzf
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  # btop
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "gruvbox_dark";
+      update_ms = 1000;
+      proc_tree = true;
+    };
+  };
+
+  # zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "docker" "docker-compose" "sudo" "git" "autojump" ];
+    };
+    shellAliases = {
+      v = "nvim";
+      b = "bat";
+      t = "tree";
+    };
+    initExtra = ''
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      test -f ~/.p10k.zsh && source ~/.p10k.zsh
+      test -f .env && export $(grep -v '^#' .env | xargs)
+    '';
+  };
+
 }
