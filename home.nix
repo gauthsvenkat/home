33ba6616fdacc4649d9d6d1b1b351dc38c0053ce
Gautham <gauthsvenkat@gmail.com>
2024-10-02 05:50:29 +0200

    Revert "tree"
    This reverts commit 5306d7121eea48dbfa09e68f4c0b7b24376db7a6.

--- home.nix
+++ home.nix
@@ -70,8 +70,6 @@
       v = "nvim";
       b = "bat";
       t = "tree";
-      ta = "tree -a -L 2";
-      lg = "lazygit";
     };
     initExtra = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
