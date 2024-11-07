c98c2844ce557aa7f72ddd882d3fce114dadf3be
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-11-07 18:06:48 +0100

    update home

--- home.nix
+++ home.nix
@@ -15,85 +15,90 @@ in
   fonts.fontconfig.enable = true;
 
   home.sessionVariables = {
-    "FLAKE" = "${homeDirectory}/.config/home-manager";
     "EDITOR" = "nvim";
   };
 
   # packages
   home.packages = with pkgs; [
-    fastfetch
     pre-commit
     tree
-    bat
     meslo-lgs-nf
-    ripgrep
     cmake
     nodejs
     rustup
     xclip
-    lazygit
     cue
     nix-output-monitor
-    nh
     nixfmt-rfc-style
+    just
   ];
 
-  # autojump
-  programs.autojump = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-  };
+  programs = {
+    fastfetch.enable = true;
+    bat.enable = true;
+    ripgrep.enable = true;
+    lazygit.enable = true;
+    direnv.enable = true;
 
-  # git
-  programs.git = {
-    enable = true;
-    userEmail = "gauthsvenkat@gmail.com";
-    userName = "Gautham";
-  };
-
-  # fzf
-  programs.fzf = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-  };
-
-  # btop
-  programs.btop = {
-    enable = true;
-    settings = {
-      color_theme = "gruvbox_dark";
-      update_ms = 1000;
-      proc_tree = true;
-    };
-  };
-
-  # zsh
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    oh-my-zsh = {
+    nh = {
       enable = true;
-      plugins = [
-        "docker"
-        "docker-compose"
-        "sudo"
-        "git"
-        "autojump"
+      flake = "${homeDirectory}/.config/home-manager";
+    };
+
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      options = [
+        "--cmd j"
       ];
     };
-    shellAliases = {
-      v = "nvim";
-      b = "bat";
-      t = "tree";
-    };
-    initExtra = ''
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      test -f ~/.p10k.zsh && source ~/.p10k.zsh
-      test -f .env && export $(grep -v '^#' .env | xargs)
-    '';
-  };
 
+    git = {
+      enable = true;
+      userEmail = "gauthsvenkat@gmail.com";
+      userName = "Gautham Venkataraman";
+    };
+
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+
+    btop = {
+      enable = true;
+      settings = {
+        color_theme = "gruvbox_dark";
+        update_ms = 1000;
+        proc_tree = true;
+      };
+    };
+
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "docker"
+          "docker-compose"
+          "sudo"
+          "git"
+          "fzf"
+          "zoxide"
+        ];
+      };
+      shellAliases = {
+        v = "nvim";
+        t = "tree";
+        b = "bat";
+      };
+      initExtra = ''
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+        test -f ~/.p10k.zsh && source ~/.p10k.zsh
+      '';
+    };
+  };
 }
